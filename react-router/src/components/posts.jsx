86ad6ccd8fdf2692a8a2d18c06c55b6cdc69bbcb
serhio <greatganymede@yandex.ru>
2023-05-25 09:54:39 +0500

    location.search

--- react-router/src/components/posts.jsx
+++ react-router/src/components/posts.jsx
@@ -1,19 +1,26 @@
 import Post from "./post";
 import PostsList from "./postsList";
+import qs from "query-string";
+import _ from "lodash";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
   const posts = [
     { id: 1, label: "post 1" },
     { id: 2, label: "post 2" },
     { id: 3, label: "post 3" },
   ];
   const postId = match.params.postId;
+  const parsed = qs.parse(location.search);
+  console.log(parsed);
+  const cropPosts = parsed.count
+    ? _(posts).slice(0).take(parsed.count).value()
+    : posts;
   return (
     <>
       {postId ? (
         <Post id={postId} posts={posts} />
       ) : (
-        <PostsList posts={posts} />
+        <PostsList posts={cropPosts} />
       )}
     </>
   );
