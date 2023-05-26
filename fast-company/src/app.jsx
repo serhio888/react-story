6aa95d7157e2b75d17dc077843a288590654018a
serhio <greatganymede@yandex.ru>
2023-05-27 03:32:23 +0500

    create main and login files

--- fast-company/src/app.jsx
+++ fast-company/src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Users from "./components/users"
 import "bootstrap/dist/css/bootstrap.css"
+import { Route, Switch } from "react-router-dom"
 
 const App = () => {
     return <Users />
