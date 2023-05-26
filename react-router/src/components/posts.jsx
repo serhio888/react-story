4e4271c5e4054af48f893c054d7cff2081bd39aa
serhio <greatganymede@yandex.ru>
2023-05-27 01:30:31 +0500

    no comments

--- react-router/src/components/posts.jsx
+++ react-router/src/components/posts.jsx
@@ -1,8 +1,6 @@
 import Post from "./post";
 import PostsList from "./postsList";
 import { useParams } from "react-router-dom";
-//import qs from "query-string";
-//import _ from "lodash";
 
 const Posts = () => {
   const posts = [
@@ -11,11 +9,6 @@ const Posts = () => {
     { id: 3, label: "post 3" },
   ];
   const { postId } = useParams();
-  //const parsed = qs.parse(location.search);
-  //console.log(parsed);
-  // const cropPosts = parsed.count
-  //   ? _(posts).slice(0).take(parsed.count).value()
-  //   : posts;
   return (
     <>
       {postId ? (
