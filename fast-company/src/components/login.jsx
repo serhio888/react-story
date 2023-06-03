957d781984d23352fd287f4854c6622b22f2b9cf
serhio <greatganymede@yandex.ru>
2023-06-03 23:43:54 +0500

    controls and uncontrols fields

--- fast-company/src/components/login.jsx
+++ fast-company/src/components/login.jsx
@@ -1,11 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const handlerChange = (e) => {
+        setEmail(e.target.value)
+    }
+
     return (
         <form action="">
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handlerChange}
+                />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
