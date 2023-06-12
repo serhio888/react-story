12f3577f34bcc8e2e5bec41162e1835141ce66b1
greatGanymede <greatGanymede@yandex.ru>
2023-06-12 10:19:07 +0500

    localStorage

--- fast-company/src/api/fake.api/user.api.js
+++ fast-company/src/api/fake.api/user.api.js
@@ -143,22 +143,37 @@ const users = [
     }
 ]
 
+if (!localStorage.getItem("users")) {
+    localStorage.setItem("users", JSON.stringify(users))
+}
+
 const fetchAll = () =>
     new Promise((resolve) => {
         window.setTimeout(function () {
-            resolve(users)
+            resolve(JSON.parse(localStorage.getItem("users")))
         }, 2000)
     })
+const update = (id, data) =>
+    new Promise((resolve) => {
+        const users = JSON.parse(localStorage.getItem("users"))
+        const userIndex = users.findIndex((u) => u._id === id)
+        users[userIndex] = { ...users[userIndex], ...data }
+        localStorage.setItem("users", JSON.stringify(users))
+        resolve(users[userIndex])
+    })
 
-const getById = (userId) => {
-    return new Promise((resolve) => {
+const getById = (id) =>
+    new Promise((resolve) => {
         window.setTimeout(function () {
-            resolve(users.find((user) => user._id === userId))
+            resolve(
+                JSON.parse(localStorage.getItem("users")).find(
+                    (user) => user._id === id
+                )
+            )
         }, 1000)
     })
-}
-
 export default {
     fetchAll,
-    getById
+    getById,
+    update
 }
